15858b5d303896e491e3c235c266c63949905ca7
Brandon Kauffman <bck01215@gmail.com>
2025-01-09 15:00:48 -0500

    platform specific rules because macos hates me

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -28,6 +28,7 @@ pub fn run() {
         },
     ];
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_clipboard::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(
--- src/lib/move.ts
+++ src/lib/move.ts
@@ -1,17 +1,26 @@
-import { getCurrentWindow, PhysicalPosition } from "@tauri-apps/api/window";
+import { getCurrentWindow, PhysicalPosition, type Window } from "@tauri-apps/api/window";
 import { cursorPosition } from "@tauri-apps/api/window";
 import { get } from "svelte/store";
+import { platform } from "@tauri-apps/plugin-os";
 import { writable } from "svelte/store";
 export async function MoveWindowToCursor() {
   const appwindow = getCurrentWindow();
-  console.log(get(scaleFactor), "scaleFactor");
+  let factor = await getFactor(appwindow);
   let coordsWindow = await cursorPosition();
   await appwindow.show();
   await appwindow.setFocus();
   coordsWindow.x = Math.round(coordsWindow.x);
   coordsWindow.y = Math.round(coordsWindow.y);
   console.log(coordsWindow);
-  await appwindow.setPosition(coordsWindow.toLogical(get(scaleFactor)));
+  await appwindow.setPosition(coordsWindow.toLogical(factor));
 }
 
-export const scaleFactor = writable(1);
+async function getFactor(appwindow: Window): Promise<number> {
+  let family = platform();
+  if (family === "macos") {
+    return get(scaleFactor);
+  }
+  return await appwindow.scaleFactor();
+}
+
+export const scaleFactor = writable(1); 
