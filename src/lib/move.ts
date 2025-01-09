9e2002d00af7fe626fd2491a8431ca9cc9efb74a
Brandon Kauffman <bck01215@gmail.com>
2025-01-09 14:08:26 -0500

    work for all

--- src/lib/move.ts
+++ src/lib/move.ts
@@ -2,11 +2,12 @@ import { getCurrentWindow, PhysicalPosition } from "@tauri-apps/api/window";
 import { cursorPosition } from "@tauri-apps/api/window";
 export async function MoveWindowToCursor() {
   const appwindow = getCurrentWindow();
+  const scaleFactor = await appwindow.scaleFactor();
   let coordsWindow = await cursorPosition();
   await appwindow.show();
   await appwindow.setFocus();
   coordsWindow.x = Math.round(coordsWindow.x);
   coordsWindow.y = Math.round(coordsWindow.y);
   console.log(coordsWindow);
-  await appwindow.setPosition(coordsWindow.toLogical(2));
+  await appwindow.setPosition(coordsWindow.toLogical(scaleFactor));
 }
